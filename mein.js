689af5191b4e4bc689aae7404879d421e9442cee
Sergey <veshtortiki@mail.ru>
2024-03-30 20:33:15 +0300

    Схранил js

---
+++ mein.js
@@ -0,0 +1,23 @@
+//Добавление настроек для блока div 
+let test_var=document.querySelector(".new");
+document.getElementById("tagFile");
+photo_var.style.width="300px";
+photo_var.style.height="300px";
+
+
+
+
+
+
+//Домашнее задание
+    function Task1(){
+    var v1=prompt("Как вас зовут?");
+    alert(`Привет,${v1}`);
+    
+    document.write("Какой то текст");
+    
+    
+
+
+    
+}
